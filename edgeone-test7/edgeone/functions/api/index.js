1cff7cfabbe1d46e8525e6693a0c2c15cc06eb42
M-ting123 <Mting11865418610@outlook.com>
2026-07-16 16:33:35 +0800

    测试

--- edgeone-test7/edgeone/functions/api/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const resp = await fetch("https://httpbin.org/get");
-    const data = await resp.json();
-    return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } });
-  }
-};
